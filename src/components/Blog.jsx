48035411bcc072714acd7cf63ab3b2a519c567bf
g1oom <albertowj2001@gmail.com>
2024-01-29 02:44:52 -0800

    Complete Blog and Post components, add images to src/images

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form, Input } from 'antd';
+import Post from './Post';
+import shuba from '../images/shuba.gif';
 
 const Blog = () => {
+    const [posts, setPosts] = useState([]);
+    const [form] = Form.useForm();
+    const { TextArea } = Input;
+
+    const onFinish = (values) => {
+        setPosts([...posts, {title: values.title, content: values.content}]);
+        form.resetFields();
+    };
+    return (
+        <div
+            style={{
+                backgroundImage: `url(${shuba})`,
+                height: "100vh",
+            }}
+        >
+            <Form
+                form={form}
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                style={{
+                    maxWidth: 600,
+                }}
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Title"
+                    name="title"
+                    rules={[
+                        {
+                        required: true,
+                        message: 'enter blog title',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Content"
+                    name="content"
+                    rules={[
+                        {
+                        required: true,
+                        message: 'enter blog content',
+                        },
+                    ]}
+                >
+                    <TextArea rows={4} />
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+
+            {posts.map((item, index) => {
+                return <Post key={index} title={item.title} content={item.content} />
+            })}
+        </div>
+    )
 
 }
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import icon from '../images/like_icon.png';
+import { Card } from 'antd';
 
-const Post = () => {
+const Post = ({title, content}) => {
+    const [likes, setLikes] = useState(0);
 
+    return (
+        <Card
+            title={title}
+            style={{
+                width: 300,
+            }}
+        >
+            <p>{content}</p>
+            <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                <button style={{margin: "10px"}}>
+                    <img src={icon} alt="my image" width="30px" height="30px" onClick={() => setLikes(likes + 1)} />
+                </button>
+                <p>{likes}</p>
+            </div>
+            
+        </Card>
+    )
 }
 
 export default Post;
